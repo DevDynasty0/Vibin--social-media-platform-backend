3b05ec307d38d16e9d60fbfc567d9f72a213ff49
ismail-hossen <satict2016@gmail.com>
2024-02-01 13:51:23 +0600

    modifyed the get posts feature for finding followings posts and rename the /get-posts api into /get-followings-posts

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,14 +1,19 @@
 import { Router } from "express";
 import {
   createPost,
+  deletePost,
   getPosts,
+  getPostsFIds,
   likeToggle,
 } from "../controllers/post.controller.js";
+import { verifyToken } from "../middelwares/auth.middleware.js";
 
 const postRouter = Router();
 
-postRouter.route("/post").post(createPost);
-postRouter.route("/posts").get(getPosts);
-postRouter.route("/like/:postId").patch(likeToggle);
+postRouter.route("/create-post").post(createPost);
+postRouter.route("/delete-post/:postId").delete(verifyToken, deletePost);
+postRouter.route("/posts-by-userId").get(verifyToken, getPosts);
+postRouter.route("/get-followings-posts").get(verifyToken, getPostsFIds);
+postRouter.route("/like/:postId").patch(verifyToken, likeToggle);
 
 export default postRouter;
