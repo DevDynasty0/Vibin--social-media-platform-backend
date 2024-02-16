94a7cf258cc847a5c4c0bf6ec5552a06f8c641ca
Foyez-Ahamed <foyez1435@gmail.com>
2024-02-16 15:37:02 +0600

    Create suspend user schema, create suspend user api and get suspend user api

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,10 +1,26 @@
 import { PostModel } from "../models/post.model.js";
+import { SuspendUser } from "../models/suspendUser.model.js";
 import { User } from "../models/user.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const getAllUsers = async (req, res) => {
   try {
-    const allUsers = await User.find({});
+    
+    const allUsers = await User.aggregate([
+      {
+        $lookup : {
+          from : "suspendusers",
+          localField : "_id",
+          foreignField : "normalUser",
+          as : "suspendedUsers"
+        }
+      },
+      {
+        $match : {
+          suspendedUsers : { $eq : [] }
+        }
+      }
+    ]);
     return res.status(200).json(new ApiResponse(200, allUsers, "all users"));
   } catch (error) {
     return res
@@ -26,4 +42,38 @@ const totalPostsCount = async (req, res) => {
   }
 };
 
-export { getAllUsers, totalPostsCount };
+
+const suspendUser = async(req, res) => {
+    try {
+      const data = {
+        normalUser: req.body.normalUser,
+        admin: req.body.admin
+      }
+     const result = await SuspendUser.create(data);
+     console.log(result);
+     return res
+      .status(200)
+      .json(new ApiResponse(200, result, "Suspend successfully"));
+
+    } catch (error) {
+      return res
+      .status(500)
+      .send({ message: "Internal Server Error", error: error.message });
+    }
+};
+
+const getSuspendedUsers = async(req, res) => {
+  try {
+    const result = await SuspendUser.find().populate("normalUser");
+    return res
+      .status(200)
+      .json(new ApiResponse(200, result, " get Suspend successfully"));
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Internal Server Error", error: error.message });
+  
+  }
+};
+
+export { getAllUsers, totalPostsCount, suspendUser, getSuspendedUsers };
