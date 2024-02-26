89d542d5a7b27b8b49b48c183b2641a7dc26e54d
abusayed0 <abusayed48876@gmail.com>
2024-02-26 22:25:28 +0600

    added genarate image by ai related routes and controller

---
+++ src/routes/vibinAI.routes.js
@@ -0,0 +1,57 @@
+import { Router } from "express";
+// import modules from OpenAI library
+import OpenAI from "openai";
+
+
+const vibinAIRouter = Router();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+vibinAIRouter.route("/genarate-caption").post(async (req, res) => {
+
+    try {
+        const { prompt } = req.body;
+
+        const completion = await openai.chat.completions.create({
+            messages: [{ "role": "user", "content": prompt }],
+            model: "gpt-3.5-turbo",
+        });
+
+        console.log(completion.choices[0].message.content);
+        return res.send({
+            caption: completion.choices[0].message.content
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+vibinAIRouter.route("/genarate-image").post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
+
+        const response = await openai.images.generate({
+            model: "dall-e-2",
+            prompt: prompt,
+            n: 1,
+            // size: "1024x1024",
+        });
+
+
+        console.log(response);
+        return res.send({
+            imageUrl: response.data
+        })
+
+
+    } catch (error) {
+        console.log(error);
+    }
+
+})
+
+export default vibinAIRouter;
