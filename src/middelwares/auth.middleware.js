0d1b351e091ff342ef85523bbb0a9e870a330d88
nadiaS11 <nadiasultana.ns11@gmail.com>
2024-03-06 10:20:13 +0600

    removed if block in verify token to check if req.coockies has accesstoken

--- src/middelwares/auth.middleware.js
+++ src/middelwares/auth.middleware.js
@@ -5,29 +5,27 @@ import { User } from "../models/user.model.js";
 
 export const verifyToken = asyncHandler(async (req, res, next) => {
   try {
-    if (req?.cookies?.accessToken) {
-      // const token = req?.cookies?.accessToken;
-      const token =
-        req?.cookies?.accessToken ||
-        req.header("Authorization")?.replace("Bearer ", "");
-      console.log(token, "token_____________ from verify token");
-      // console.log("request users cookies", req?.cookies);
-      if (!token) {
-        throw new ApiError(401, "Unauthorized request.");
-      }
-
-      const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-      const user = await User.findById(decodeToken._id).select("-password  ");
-
-      if (!user) {
-        throw new ApiError(401, "Invalid access token.");
-      }
-
-      req.user = user;
-
-      next();
+    // const token = req?.cookies?.accessToken;
+    const token =
+      req?.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+    console.log(token, "token_____________ from verify token");
+    // console.log("request users cookies", req?.cookies);
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request.");
     }
+
+    const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodeToken._id).select("-password  ");
+
+    if (!user) {
+      throw new ApiError(401, "Invalid access token.");
+    }
+
+    req.user = user;
+
+    next();
   } catch (error) {
     throw new ApiError(401, error?.message || "Invalid access token.");
   }
